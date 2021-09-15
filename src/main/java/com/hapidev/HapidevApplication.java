e9df2be78a4acaa6b2d3ffc3bfc5fd8b0bc9e396
sunny <sunny@sunnyx.local>
2021-09-15 09:26:07 +0800

    Initial commit

---
+++ src/main/java/com/hapidev/HapidevApplication.java
@@ -0,0 +1,13 @@
+package com.hapidev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HapidevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HapidevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hapidev/HapidevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hapidev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HapidevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
